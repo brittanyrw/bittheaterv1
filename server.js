b73d07dc5760c95744cf7d3e294ecb8b1b9a5f42
Brittany Walker <breneewalker@gmail.com>
2017-04-14 12:21:59 -0400

    remove comment about formidable

--- server.js
+++ server.js
@@ -39,9 +39,6 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-//parsing form data
-
-
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
