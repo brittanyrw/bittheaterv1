d6c6ee11e74ada260b32926c8842720198d32572
Brittany Walker <breneewalker@gmail.com>
2017-05-04 18:22:03 -0400

    update review schema

--- app/models/review.js
+++ app/models/review.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var reviewSchema = mongoose.Schema({
 
-       	userid        : String, //req.user._id
+       	userId        : String, //req.user._id
         published     : {type: Date, default: Date.now},
         reviewTitle   : String,
         reviewContent : String,
