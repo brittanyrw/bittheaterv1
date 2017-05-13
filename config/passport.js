46f71183cc294ce94cfdd4afebc597f5e7045544
Brittany Walker <breneewalker@gmail.com>
2017-05-13 15:50:33 -0400

    update

--- config/passport.js
+++ config/passport.js
@@ -96,7 +96,6 @@ module.exports = function(passport) {
 
                         newUser.local.email    = email;
                         newUser.local.password = newUser.generateHash(password);
-
                         newUser.save(function(err) {
                             if (err)
                                 return done(err);
