6577189065d49042f301a1f5587eab4287658cfc
Brittany Walker <breneewalker@gmail.com>
2017-05-14 20:37:46 -0400

    rename badge suggestion schema

--- app/models/badge-suggestion.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var badgeSuggestionSchema = mongoose.Schema({
-
-        name         : String,
-        suggestion   : String
-});
-
-module.exports = mongoose.model('BadgeSuggestion', badgeSuggestionSchema);
