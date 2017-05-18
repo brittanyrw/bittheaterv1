3cdd5c794eb3aa5c3ef5888d57a2f70767a1e774
Brittany Walker <breneewalker@gmail.com>
2017-05-18 13:12:29 -0400

    update gitignore file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://admin:1234@ds141410.mlab.com:41410/bittheater' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://admin:1234@ds141410.mlab.com:41410/bittheater' 
 
 };
